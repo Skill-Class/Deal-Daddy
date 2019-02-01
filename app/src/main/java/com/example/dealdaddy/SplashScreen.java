49e9e97a5699a1e603ee7802547c4bf794f8d54a
sheetalkumar <sheetal.mca17.du@gmail.com>
2019-02-02 01:31:19 +0530

    animation in splash screen

--- app/src/main/java/com/example/dealdaddy/SplashScreen.java
+++ app/src/main/java/com/example/dealdaddy/SplashScreen.java
@@ -3,13 +3,30 @@ package com.example.dealdaddy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private ImageView logo;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+        logo = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+
+        Animation animationLogo= AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoom_exit);
+        Animation animationText= AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom_to_up);
+
+        logo.startAnimation(animationLogo);
+        textView.startAnimation(animationText);
 
     }
 
