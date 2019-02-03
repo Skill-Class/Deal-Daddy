704a8c28a73c9c36eef76fface59c3b7888fc7d8
sheetalkumar <sheetal.mca17.du@gmail.com>
2019-02-04 00:49:14 +0530

    Order Summery added.

--- app/src/main/java/com/example/dealdaddy/Activity/OrderConfirmActivity.java
+++ app/src/main/java/com/example/dealdaddy/Activity/OrderConfirmActivity.java
@@ -7,7 +7,11 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.dealdaddy.Adapter.RecyclerViewAdapterForItems;
 import com.example.dealdaddy.Adapter.RecyclerViewAdapterForItemsDetails;
@@ -23,12 +27,23 @@ public class OrderConfirmActivity extends AppCompatActivity {
     ImageView BackBtn;
 
     private List<Items> itemsDetails;
+
+    private Spinner spinnerSize;
+    private Spinner spinnerQty;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_confirm);
 
         BackBtn = findViewById(R.id.navicon);
+        spinnerSize = findViewById(R.id.spinner);
+
+        //Spinner will be added later.
+        // it will be implemented in Order Confirm adapter class.
+
+
+
+
 
         BackBtn.setOnClickListener(new View.OnClickListener() {
             @Override
