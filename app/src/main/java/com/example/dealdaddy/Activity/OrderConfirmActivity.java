3988b45a69c0a8be5b6e796e10a9505fc83e5dfb
sheetalkumar <sheetal.mca17.du@gmail.com>
2019-02-04 00:39:07 +0530

    Order Summery added.

--- app/src/main/java/com/example/dealdaddy/Activity/OrderConfirmActivity.java
+++ app/src/main/java/com/example/dealdaddy/Activity/OrderConfirmActivity.java
@@ -3,15 +3,26 @@ package com.example.dealdaddy.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.example.dealdaddy.Adapter.RecyclerViewAdapterForItems;
+import com.example.dealdaddy.Adapter.RecyclerViewAdapterForItemsDetails;
+import com.example.dealdaddy.Adapter.RecyclerViewAdapterForOrderConfirm;
+import com.example.dealdaddy.Model.Items;
 import com.example.dealdaddy.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderConfirmActivity extends AppCompatActivity {
 
     ImageView BackBtn;
 
+    private List<Items> itemsDetails;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +37,36 @@ public class OrderConfirmActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        itemsDetails = new ArrayList<>();
+
+
+        itemsDetails.add(new Items("BlackBerrys", "Men Slim FIt Casual Shirt",
+                "\\u20B9 1245  \\u20B9 2490 (40% Off)", false));
+
+        itemsDetails.add(new Items("BlackBerrys", "Men Slim FIt Casual Shirt",
+                "\\u20B9 1245  \\u20B9 2490 (40% Off)", false));
+
+        itemsDetails.add(new Items("BlackBerrys", "Men Slim FIt Casual Shirt",
+                "\\u20B9 1245  \\u20B9 2490 (40% Off)", false));
+
+        itemsDetails.add(new Items("BlackBerrys", "Men Slim FIt Casual Shirt",
+                "\\u20B9 1245  \\u20B9 2490 (40% Off)", false));
+
+        itemsDetails.add(new Items("BlackBerrys", "Men Slim FIt Casual Shirt",
+                "\\u20B9 1245  \\u20B9 2490 (40% Off)", false));
+
+
+
+        RecyclerView myrv = (RecyclerView) findViewById(R.id.recyclerView);
+        RecyclerViewAdapterForOrderConfirm myAdapter = new RecyclerViewAdapterForOrderConfirm(this, itemsDetails);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        myrv.setLayoutManager(layoutManager);
+        myrv.setAdapter(myAdapter);
+
 
     }
 
