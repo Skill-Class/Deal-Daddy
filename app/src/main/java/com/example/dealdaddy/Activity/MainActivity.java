3e240e285a34a3f81cb883fd3c5dd68176ef82a9
sheetalkumar <sheetal.mca17.du@gmail.com>
2019-02-05 00:13:09 +0530

    home screen changed.

--- app/src/main/java/com/example/dealdaddy/Activity/MainActivity.java
+++ app/src/main/java/com/example/dealdaddy/Activity/MainActivity.java
@@ -43,44 +43,46 @@ public class MainActivity extends AppCompatActivity {
     private List<ItemsWithImage> itemsDetails;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-
         itemsDetails = new ArrayList<>();
+
+        bagImage = findViewById(R.id.imageView7);
+        navicon = findViewById(R.id.navicon);
+        bottomTextView = findViewById(R.id.bottomTextView);
+        viewLast = findViewById(R.id.viewlast);
 
         /*
         Here
         Company name = Categories
          */
 
-        itemsDetails.add(new ItemsWithImage(R.drawable.pic2,"Men", "Men Slim FIt Casual Shirt",
+        itemsDetails.add(new ItemsWithImage(R.drawable.pic2, "Men", "Men Slim FIt Casual Shirt",
                 "\\u20B9 1245  \\u20B9 2490 (40% Off)", false));
 
-        itemsDetails.add(new ItemsWithImage(R.drawable.pic3,"Women", "Men Slim FIt Casual Shirt",
+        itemsDetails.add(new ItemsWithImage(R.drawable.pic3, "Women", "Men Slim FIt Casual Shirt",
                 "\\u20B9 1245  \\u20B9 2490 (40% Off)", false));
 
-        itemsDetails.add(new ItemsWithImage(R.drawable.pic4,"Kids", "Men Slim FIt Casual Shirt",
+        itemsDetails.add(new ItemsWithImage(R.drawable.pic4, "Kids", "Men Slim FIt Casual Shirt",
                 "\\u20B9 1245  \\u20B9 2490 (40% Off)", false));
 
-        itemsDetails.add(new ItemsWithImage(R.drawable.pic3,"Furniture", "Men Slim FIt Casual Shirt",
+        itemsDetails.add(new ItemsWithImage(R.drawable.pic3, "Furniture", "Men Slim FIt Casual Shirt",
                 "\\u20B9 1245  \\u20B9 2490 (40% Off)", false));
 
-      /*  menImageView = findViewById(R.id.imageView1);
-        womenImageView = findViewById(R.id.imageView2);
-        boysImageView = findViewById(R.id.imageView3);
-        kidsImageView = findViewById(R.id.imageView4);
-        girlsImageView = findViewById(R.id.imageView6);
-        furnitureImageView = findViewById(R.id.imageView5);
-        bagImage = findViewById(R.id.imageView7);
-        navicon = findViewById(R.id.navicon);
+        //menImageView = findViewById(R.id.imageView1);
+        //womenImageView = findViewById(R.id.imageView2);
+        //boysImageView = findViewById(R.id.imageView3);
+        //kidsImageView = findViewById(R.id.imageView4);
+        // girlsImageView = findViewById(R.id.imageView6);
+        //  furnitureImageView = findViewById(R.id.imageView5);
 
-        bottomTextView=findViewById(R.id.bottomTextView);
-        viewLast = findViewById(R.id.viewlast);
+
+      /*
+
 
 
         cardViewone = findViewById(R.id.cardView1);
@@ -92,14 +94,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.right_to_left);
-        Animation animationNav = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.left_to_right);
-        Animation animationBottom = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom_to_up);
 
-        bagImage.startAnimation(animation);
-        navicon.startAnimation(animationNav);
-        viewLast.startAnimation(animationBottom);
-        bottomTextView.startAnimation(animationBottom);
 
 
         String menUrl = "https://firebasestorage.googleapis.com/v0/b/dealdaddy-9622a.appspot.com/o/images%2Fmenimg.jpg?alt=media&token=2087538c-18e5-4dea-96bd-70c157020a84";
@@ -136,6 +131,14 @@ public class MainActivity extends AppCompatActivity {
                 */
 
 
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.right_to_left);
+        Animation animationNav = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.left_to_right);
+        Animation animationBottom = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom_to_up);
+
+        bagImage.startAnimation(animation);
+        navicon.startAnimation(animationNav);
+        viewLast.startAnimation(animationBottom);
+        bottomTextView.startAnimation(animationBottom);
 
         RecyclerView myrv = (RecyclerView) findViewById(R.id.recyclerView);
         RecyclerViewAdapterForHomeScreen myAdapter = new RecyclerViewAdapterForHomeScreen(this, itemsDetails);
