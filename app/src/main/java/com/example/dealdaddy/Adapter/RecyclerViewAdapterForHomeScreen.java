e8e923af1015104e2eb0155dabba9f0d37bba9de
MEGHNA AGARWAL <31645367+meghna125@users.noreply.github.com>
2019-02-08 01:37:33 +0530

    done (#18)

--- app/src/main/java/com/example/dealdaddy/Adapter/RecyclerViewAdapterForHomeScreen.java
+++ app/src/main/java/com/example/dealdaddy/Adapter/RecyclerViewAdapterForHomeScreen.java
@@ -1,6 +1,5 @@
 package com.example.dealdaddy.Adapter;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -53,63 +52,26 @@ public class RecyclerViewAdapterForHomeScreen extends RecyclerView.Adapter<Recyc
         holder.modelImage.setImageResource(mData.get(position).getProductImage());
         holder.companyName.setText(mData.get(position).getCompanyName());
 
-      /*  holder.modelImage.setOnClickListener(new View.OnClickListener() {
+        holder.modelImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(mContext, DetailActivity.class);
                 mContext.startActivity(intent);
             }
-        });*/
+        });
 
         holder.modelImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(mContext, DetailActivity.class);
+                Intent intent = new Intent(mContext,DetailActivity.class);
+
                 Bundle bundle = new Bundle();
-                bundle.putString("ModelType", mData.get(position).getCompanyName());
-                bundle.putInt("ModelImage", mData.get(position).getProductImage());
+                bundle.putString("ModelType",mData.get(position).getCompanyName());
+                bundle.putInt("ModelImage",mData.get(position).getProductImage());
                 intent.putExtras(bundle);
-               // mContext.(R.anim.slide_in_right,R.anim.slide_out_right);
-               // startActivity(intent_next);
-               // finish();
-                Activity activity = (Activity) mContext;
-                activity.startActivity(intent);
-                activity.overridePendingTransition(R.anim.zoom_exit,R.anim.slide_out_right);
 
             }
         });
-
-
-        if (position == 0) {
-
-            holder.modelFirstImage.setImageResource(mData.get(position).getItemForHomeImages().get(0).getMenImage());
-            holder.modelSecondImage.setImageResource(mData.get(position).getItemForHomeImages().get(1).getMenImage());
-            holder.modelThirdImage.setImageResource(mData.get(position).getItemForHomeImages().get(2).getMenImage());
-            holder.modelFourthImage.setImageResource(mData.get(position).getItemForHomeImages().get(3).getMenImage());
-
-        }if(position ==1){
-            holder.modelFirstImage.setImageResource(mData.get(position).getItemForHomeImages().get(0).getWomenImage());
-            holder.modelSecondImage.setImageResource(mData.get(position).getItemForHomeImages().get(1).getWomenImage());
-            holder.modelThirdImage.setImageResource(mData.get(position).getItemForHomeImages().get(2).getWomenImage());
-            holder.modelFourthImage.setImageResource(mData.get(position).getItemForHomeImages().get(3).getWomenImage());
-
-        }
-        if(position ==2){
-            holder.modelFirstImage.setImageResource(mData.get(position).getItemForHomeImages().get(0).getKidsImage());
-            holder.modelSecondImage.setImageResource(mData.get(position).getItemForHomeImages().get(1).getKidsImage());
-            holder.modelThirdImage.setImageResource(mData.get(position).getItemForHomeImages().get(2).getKidsImage());
-            holder.modelFourthImage.setImageResource(mData.get(position).getItemForHomeImages().get(3).getKidsImage());
-
-        }
-        if(position ==3){
-            holder.modelFirstImage.setImageResource(mData.get(position).getItemForHomeImages().get(0).getFurnitureImage());
-            holder.modelSecondImage.setImageResource(mData.get(position).getItemForHomeImages().get(1).getFurnitureImage());
-            holder.modelThirdImage.setImageResource(mData.get(position).getItemForHomeImages().get(2).getFurnitureImage());
-            holder.modelFourthImage.setImageResource(mData.get(position).getItemForHomeImages().get(3).getFurnitureImage());
-
-        }
-
-
         /* holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -136,25 +98,18 @@ public class RecyclerViewAdapterForHomeScreen extends RecyclerView.Adapter<Recyc
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView priceText, companyName;
-        ImageView favImageView, modelImage;
-
-        ImageView modelFirstImage, modelSecondImage, modelThirdImage, modelFourthImage;
-
+        TextView priceText,companyName;
+        ImageView favImageView,modelImage;
 
         // CardView cardView ;
 
         public MyViewHolder(View itemView) {
             super(itemView);
 
-            // priceText = (TextView) itemView.findViewById(R.id.textView9);
+           // priceText = (TextView) itemView.findViewById(R.id.textView9);
             //favImageView = itemView.findViewById(R.id.imageView10);
             modelImage = itemView.findViewById(R.id.imageView9);
             companyName = itemView.findViewById(R.id.textView7);
-            modelFirstImage = itemView.findViewById(R.id.imageView91);
-            modelSecondImage = itemView.findViewById(R.id.imageView911);
-            modelThirdImage = itemView.findViewById(R.id.imageView9111);
-            modelFourthImage = itemView.findViewById(R.id.imageView91111);
 
             // String text = "\\u20B9 1245  \\u20B9 2490 (50% Off)";
 
