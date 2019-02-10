cf4394a8813cf15eb005eb5522e04f0e91cee958
MEGHNA AGARWAL <31645367+meghna125@users.noreply.github.com>
2019-02-10 21:58:38 +0530

    new changes (#21)
    * done
    
    * done

--- app/src/main/java/com/example/dealdaddy/Fragment/LoginFragment.java
+++ app/src/main/java/com/example/dealdaddy/Fragment/LoginFragment.java
@@ -1,12 +1,20 @@
 package com.example.dealdaddy.Fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
+import com.example.dealdaddy.Activity.MainActivity;
 import com.example.dealdaddy.R;
 
 /**
@@ -19,12 +27,41 @@ public class LoginFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private TextView dontHaveAnAccount;
+    private FrameLayout parentFrameLayout;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        dontHaveAnAccount = view.findViewById(R.id.signup);
+        parentFrameLayout = getActivity().findViewById(R.id.RegisterFrameLayout);
+
+        return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        dontHaveAnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                setFragment(new SignUpFragment());
+            }
+        });
+
+
+    }
+
+    private void setFragment(Fragment fragment){
+
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(parentFrameLayout.getId(),fragment);
+        fragmentTransaction.commit();
+    }
 }
